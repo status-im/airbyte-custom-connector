11efa928114c01a57fe9ebcb07ba0970d5b86b86
Alexis Pentori <alexis@status.im>
2024-03-27 15:56:48 +0100

    twitter-fetcher: update connector
    Signed-off-by: Alexis Pentori <alexis@status.im>

--- source-twitter-fetcher/source_twitter_fetcher/source.py
+++ source-twitter-fetcher/source_twitter_fetcher/source.py
@@ -12,127 +12,85 @@ from datetime import datetime
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
 from airbyte_cdk.sources.streams.http import HttpStream, HttpSubStream
-from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator
+from airbyte_cdk.sources.streams.http.auth.core import HttpAuthenticator
+from airbyte_cdk.sources.streams.http.requests_native_auth import SingleUseRefreshTokenOauth2Authenticator
 
 logger = logging.getLogger("airbyte")
 
 class TwitterStream(HttpStream):
 
-  url_base = "https://api.twitter.com/2/"
+    url_base = "https://api.twitter.com/2/"
 
-  def __init__(self, api_key: str=None, accounts: List=None, start_time: str = None, stop_time: str = None, **kwargs):
-    super().__init__(**kwargs)
-    self.api_key = api_key
-    self.accounts = accounts
-    self.start_time = start_time
-    self.stop_time = stop_time;
+    def __init__(self, start_time: str = None, stop_time: str = None, **kwargs):
+        super().__init__(**kwargs)
+        self.start_time = start_time
+        self.stop_time = stop_time;
 
-  def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
-    return None
-
-class TwitterAccountData(TwitterStream):
-
-  primary_key = "account_id"
-
-  @property
-  def use_cache(self) -> bool:
-    return True
+    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+        return None
 
 
-  def stream_slices(self, **kwargs) -> Iterable[Optional[Mapping[str, Any]]]:
-    for account in self.accounts:
-      yield {
-        "name": account
-      }
+class Account(TwitterStream):
+
+    primary_key = "id"
+
+    @property
+    def use_cache(self) -> bool:
+        return True
+
+    def path(
+          self, stream_state: Mapping[str, Any] = None, 
+          stream_slice: Mapping[str, Any] = None, 
+          next_page_token: Mapping[str, Any] = None
+          ) -> str:
+        return f"users/me?user.fields=public_metrics"
+
+    def parse_response(
+          self, 
+          response: requests.Response,
+          stream_slice: Mapping[str, Any] = None,
+          **kwargs
+    ) -> Iterable[Mapping]:
+        logger.info("Response: %s", response.json())
+        data=response.json()['data']
+        yield data
+        # Wait to avoid reaching API limit
+        time.sleep(2)
 
 
-  def path(
-    self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
-  ) -> str:
-    return f"users/by/username/{stream_slice['name']}?user.fields=public_metrics"
+class Tweet(HttpSubStream, Account):
+    primary_key = "id"
 
-  def request_headers(
-    self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
-  ) -> MutableMapping[str, Any]:
-    return { 
-      "Authorization" : f"Bearer {self.api_key}",
-      "User-Agent": "v2RecentSearchPython"
-    }
+    def __init__(self, **kwargs):
+        super().__init__(Account(**kwargs),**kwargs)
 
-  def parse_response(
-        self, 
-        response: requests.Response,
-        stream_slice: Mapping[str, Any] = None,
-        **kwargs
-  ) -> Iterable[Mapping]:
-    logger.info("Getting data of %s account", stream_slice['name'])
-    logger.info("Response: %s", response.json())
-    data=response.json()['data']
-    yield {
-      "account_id": data['id'],
-      "username": data['username'],
-      "account_name": data['name'],
-      "tweet_count": data['public_metrics']['tweet_count'],
-      "like_count": data['public_metrics']['like_count'],
-      "following_count": data['public_metrics']['following_count'],
-      "follower_count": data['public_metrics']['followers_count'],
-      "listed_count": data['public_metrics']['listed_count'],
-    }
-    time.sleep(2)
-
-
-class TwitterTweet(HttpSubStream, TwitterAccountData):
-    #TODO: See how to get the account ID
-  primary_key = ""
-  def __init__(self, **kwargs):
-    super().__init__(TwitterAccountData(**kwargs),**kwargs)
-
-  def path(
-    self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
-  ) -> str:
-        account_id = stream_slice.get("parent").get("account_id")
-        return f"users/{account_id}/tweets?tweet.fields=text,public_metrics,author_id,referenced_tweets,created_at&start_time={self.start_time}"
-
-  def request_headers(
-    self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
-  ) -> MutableMapping[str, Any]:
-    return { 
-      "Authorization" : f"Bearer {self.api_key}",
-      "User-Agent": "v2RecentSearchPython"
-    }
-
-  def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
-    data=response.json()['data']
-    logger.info("Response: %s", response.json())
-    referenced_tweets=""
-    for t in data:
-      if "referenced_tweets" in t:
-        for rt in t.get('referenced_tweets'):
-          referenced_tweets += f"{rt.get('type')}:{rt.get('id')};"
-      yield {
-        "id": t['id'],
-        "created_at": t.get('created_at'),
-        "retweet_count": t.get('public_metrics').get('retweet_count'),
-        "reply_count": t.get('public_metrics').get('reply_count'),
-        "like_count": t.get('public_metrics').get('like_count'),
-        "quote_count": t.get('public_metrics').get('quote_count'),
-        "referenced_tweets": referenced_tweets
-      }
-    time.sleep(2)
+    def path(
+          self, stream_state: Mapping[str, Any] = None, 
+          stream_slice: Mapping[str, Any] = None, 
+          next_page_token: Mapping[str, Any] = None
+    ) -> str:
+        account_id = stream_slice.get("parent").get("id")
+        logger.info("Account id %s", account_id)
+        return f"users/{account_id}/tweets?tweet.fields=text,public_metrics,non_public_metrics,organic_metrics,author_id,referenced_tweets,created_at"
+    
+    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        logger.debug("Twtter Response: %s", response.json())
+        data=response.json()['data']
+        for t in data:
+            yield t
+        time.sleep(2)
 
 # Source
 class SourceTwitterFetcher(AbstractSource):
-  def check_connection(self, logger, config) -> Tuple[bool, any]:
-    return True, None
+    def check_connection(self, logger, config) -> Tuple[bool, any]:
+        return True, None
 
-  def streams(self, config: Mapping[str, Any]) -> List[Stream]:       
-    return [
-      TwitterAccountData(
-        api_key=config['api_key'],
-        accounts=config['accounts']),
-      TwitterTweet(
-        api_key=config['api_key'],
-        accounts=config['accounts'],
-        start_time=config['start_time'], 
-        stop_time=datetime.now().isoformat())
-    ]
+    def streams(self, config: Mapping[str, Any]) -> List[Stream]:
+        auth=SingleUseRefreshTokenOauth2Authenticator(
+                config, token_refresh_endpoint="https://api.twitter.com/2/oauth2/token") 
+        return [
+            Account(authenticator=auth),
+            Tweet(authenticator=auth, start_time=config['start_time'],
+                stop_time=datetime.now().isoformat()
+            )
+        ]
