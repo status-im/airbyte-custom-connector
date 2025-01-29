ec909e8c2decf64028ab7d4e956a627417c91a05
c200bzh <claire.pinson200@gmail.com>
2025-01-29 16:05:48 +0800

    twitter : adding streams from x_ads_api to get engagement on promoted tweets and active promoted tweets ids

--- source-twitter-fetcher/source_twitter_fetcher/source.py
+++ source-twitter-fetcher/source_twitter_fetcher/source.py
@@ -5,7 +5,7 @@ from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
 
 from .tweets_stream import Account, Tweet, TweetMetrics, TweetPromoted
-from .ads_stream import PromotedTweetBilling
+from .ads_stream import PromotedTweetActive, PromotedTweetBilling, PromotedTweetEngagement
 from .auth import TwitterOAuth
 
 class SourceTwitterFetcher(AbstractSource):
@@ -36,16 +36,31 @@ class SourceTwitterFetcher(AbstractSource):
             parent=tweet
         )
 
+        promoted_tweet_active = PromotedTweetActive(
+            authenticator=auth,
+            account_id=config['account_id'],
+            start_time=datetime.strptime(config['start_time'], "%Y-%m-%dT%H:%M:%SZ"),
+        )
+
         promoted_tweet_billing = PromotedTweetBilling(
             authenticator=auth,
             account_id=config['account_id'],
             parent=tweet_promoted
         )
+
+        promoted_tweet_engagement = PromotedTweetEngagement(
+            authenticator=auth,
+            account_id=config['account_id'],
+            parent=promoted_tweet_active
+        )
+
         
         return [
             Account(authenticator=auth, account_id=config["account_id"]),
             tweet,
             tweet_metrics,
             tweet_promoted,
-            promoted_tweet_billing
+            promoted_tweet_active,
+            promoted_tweet_billing,
+            promoted_tweet_engagement
         ]
